c4dd55a90d19620aa08720acb5321a69bc1da753
RBrunning <you@example.com>
2024-10-10 12:13:37 +0100

    3.13 and 3.14: Integrate MongoDB for fetching, saving, deleting, and updating phonebook entries

--- index.js
+++ index.js
@@ -1,67 +1,61 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
+const Entry = require('./models/entry')
 
 const app = express()
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
 
 app.get('/api/people', (req, res) => {
-  res.json(persons)
+  Entry.find({}).then(entries => {
+    res.json(entries)
+  })
 })
 
 app.get('/api/people/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(person => person.id === id)
-  person ? res.json(person) : res.status(404).end()
+  Entry.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ error: 'Something went wrong' })
+    })
 })
 
 app.get('/info', (req, res) => {
-  const currentDate = new Date(); 
-  res.send(`
-    <p>Phonebook has info for ${persons.length} people.</p>
-    <p>${currentDate}</p>
-  `)
+  Entry.countDocuments({})
+    .then(count => {
+      const currentDate = new Date()
+      res.send(`
+        <p>Phonebook has info for ${count} people.</p>
+        <p>${currentDate}</p>
+      `)
+    })
 })
 
 app.delete('/api/people/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+  Entry.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ error: 'Something went wrong' })
+    })
 })
 
 app.post('/api/people', (req, res) => {
@@ -73,28 +67,61 @@ app.post('/api/people', (req, res) => {
     })
   }
 
-  if (persons.map((person) => person.name).includes(body.name)) {
-    return res.status(400).json({ 
-      error: 'person already in phonebook' 
-    })
-  }
-
   if (!body.number) {
     return res.status(400).json({ 
       error: 'number missing' 
     })
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
-    id: Math.random().toString()
-  }
+  Entry.findOne({ name: body.name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return res.status(400).json({ error: 'person already in phonebook' })
+      } else {
+        const person = new Entry({
+          name: body.name,
+          number: body.number,
+        })
 
-  persons = persons.concat(person)
-  res.json(person)
+        person.save().then(savedPerson => {
+          res.json(savedPerson)
+        })
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(500).send({ error: 'Something went wrong' })
+    })
 })
 
+app.put('/api/people/:id', (req, res) => {
+  const { name, number } = req.body
+  const id = req.params.id
+
+  if (!number) {
+    return res.status(400).json({ error: 'Number is missing' })
+  }
+
+  const updatedPerson = { name, number }
+
+  Entry.findByIdAndUpdate(id, updatedPerson, { new: true, runValidators: true, context: 'query' })
+    .then(updatedEntry => {
+      if (updatedEntry) {
+        res.json(updatedEntry)
+      } else {
+        res.status(404).json({ error: 'Person not found' })
+      }
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(400).json({ error: 'Invalid ID format' })
+    })
+})
+
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
